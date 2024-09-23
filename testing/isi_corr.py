0bad4a7238e4b81a1913f8d8432fbb4be2b7a3b9
suyash144 <suyashag@gmail.com>
2024-09-23 15:04:51 +0100

    started new script to test ISI correlations

---
+++ testing/isi_corr.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import os
+
+test_data_root = r"C:\Users\suyas\R_DATA_UnitMatch"
+
+def compare_isi(test_data_root, mouse, probe, loc):
+    mt_path = os.path.join(test_data_root, mouse, probe, loc, "new_matchtable.csv")
+    if not os.path.exists(mt_path):
+        raise ValueError("Matchtable not found for (mouse, probe, loc: {mouse}, {probe}, {loc})")
+    mt = pd.read_csv(mt_path)
+    matches = np.sqrt(len(mt))
+    if (matches).is_integer():
+        percentile = 1 - matches/len(mt)
+    else:
+        raise ValueError("Length of matchtable is not (no. of neurons)^2")
+    thresh = mt.quantile(percentile, )
+    mt = mt.loc[mt["DNNSim" > thresh], ["ISICorr", "DNNSim", "DNNProb"]]
+
+
+compare_isi(test_data_root, "AL031", "19011116684", "1")
