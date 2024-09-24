f5bc1d3f96af9b8fc28262d904ba12fcad0b1734
suyash144 <suyashag@gmail.com>
2024-09-24 12:54:50 +0100

    isi correlation script working

--- testing/isi_corr.py
+++ testing/isi_corr.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 test_data_root = r"C:\Users\suyas\R_DATA_UnitMatch"
 
@@ -11,11 +12,13 @@ def compare_isi(test_data_root, mouse, probe, loc):
     mt = pd.read_csv(mt_path)
     matches = np.sqrt(len(mt))
     if (matches).is_integer():
-        percentile = 1 - matches/len(mt)
+        matches = int(matches)
     else:
         raise ValueError("Length of matchtable is not (no. of neurons)^2")
-    thresh = mt["DNNSim"].quantile(percentile)
-    mt = mt.loc[mt["DNNSim" > thresh], ["ISICorr", "DNNSim", "DNNProb"]]
+    sorted = mt.sort_values(by = "DNNSim", ascending=False).head(matches)
+    sorted = sorted.loc[:, ["DNNSim", "ISICorr"]]
+    plt.hist(sorted["ISICorr"], bins = 100)
+    plt.show()
 
 
 compare_isi(test_data_root, "AL031", "19011116684", "1")
