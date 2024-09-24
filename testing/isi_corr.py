500f3bc0faeaa063074c9b5d1c3c0073547b6794
suyash144 <suyashag@gmail.com>
2024-09-24 13:28:13 +0100

    can now just pass in path to match table

--- testing/isi_corr.py
+++ testing/isi_corr.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 
 test_data_root = r"C:\Users\suyas\R_DATA_UnitMatch"
 
-def compare_isi(test_data_root, mouse, probe, loc):
-    mt_path = os.path.join(test_data_root, mouse, probe, loc, "new_matchtable.csv")
+def compare_isi(mt_path:str):
+    """
+    Pass in path to the match table csv file.
+    Shows the ISI correlation histogram for that set of experiments.
+    """
     if not os.path.exists(mt_path):
-        raise ValueError(f"Matchtable not found for (mouse, probe, loc: {mouse}, {probe}, {loc})")
+        raise ValueError(f"Matchtable not found at {mt_path}")
     mt = pd.read_csv(mt_path)
     matches = np.sqrt(len(mt))
     if (matches).is_integer():
@@ -26,4 +29,5 @@ def compare_isi(test_data_root, mouse, probe, loc):
 
 # compare_isi(test_data_root, "AL031", "19011116684", "1")
 # compare_isi(test_data_root, "AL032", "19011111882", "2")
-compare_isi(test_data_root, "AL036", "19011116882", "3")
+mt_path = os.path.join(test_data_root, "AL036", "19011116882", "3", "new_matchtable.csv")
+compare_isi(mt_path)
