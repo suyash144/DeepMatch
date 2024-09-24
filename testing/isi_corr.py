d7127cbd03087c1b0ff5da055b4b4ed810bac938
suyashagarwal138 <suyashag@gmail.com>
2024-09-24 11:00:57 +0100

    fixes

--- testing/isi_corr.py
+++ testing/isi_corr.py
@@ -7,14 +7,14 @@ test_data_root = r"C:\Users\suyas\R_DATA_UnitMatch"
 def compare_isi(test_data_root, mouse, probe, loc):
     mt_path = os.path.join(test_data_root, mouse, probe, loc, "new_matchtable.csv")
     if not os.path.exists(mt_path):
-        raise ValueError("Matchtable not found for (mouse, probe, loc: {mouse}, {probe}, {loc})")
+        raise ValueError(f"Matchtable not found for (mouse, probe, loc: {mouse}, {probe}, {loc})")
     mt = pd.read_csv(mt_path)
     matches = np.sqrt(len(mt))
     if (matches).is_integer():
         percentile = 1 - matches/len(mt)
     else:
         raise ValueError("Length of matchtable is not (no. of neurons)^2")
-    thresh = mt["DNNSim"].quantile(percentile, numeric_only=True)
+    thresh = mt["DNNSim"].quantile(percentile)
     mt = mt.loc[mt["DNNSim" > thresh], ["ISICorr", "DNNSim", "DNNProb"]]
 
 
