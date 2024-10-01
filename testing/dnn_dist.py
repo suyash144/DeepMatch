c6d7b18f540033b63ff2d02136ff9deef1132bf1
suyash144 <suyashag@gmail.com>
2024-10-01 19:25:04 +0100

    computing threshold value

--- testing/dnn_dist.py
+++ testing/dnn_dist.py
@@ -14,16 +14,22 @@ def dnn_sim_dist(mt_path):
     # sanity check that the categories are being loaded correctly
     assert sum(on_diag.index.isin(off_diag.index)) == 0
 
-    weights_on = np.ones_like(on_diag) / len(on_diag)
-    weights_off = np.ones_like(off_diag) / len(off_diag)
     plt.hist(on_diag["DNNSim"], bins = 500, alpha = 0.5, density=True, label="On diagonal")
     plt.hist(off_diag["DNNSim"], bins = 500, alpha = 0.5, density=True, label="Off diagonal")
     kde_on = KernelDensity(kernel='gaussian', bandwidth=0.01).fit(on_diag)
     kde_off = KernelDensity(kernel='gaussian', bandwidth=0.01).fit(off_diag)
     x = np.linspace(min(off_diag["DNNSim"]), max(on_diag["DNNSim"]), 1000).reshape(-1, 1)
-    plt.plot(x, np.exp(kde_on.score_samples(x)))
-    plt.plot(x, np.exp(kde_off.score_samples(x)))
+    y_on = np.exp(kde_on.score_samples(x))
+    y_off = np.exp(kde_off.score_samples(x))
+    plt.plot(x, y_on, label="On diagonal")
+    plt.plot(x, y_off, label="Off diagonal")
 
+    # Find the threshold where the distributions intersect
+    thresh=np.argwhere(np.diff(np.sign(y_off - y_on)))
+    print(f"Threshold: {x[thresh].item()}")
+    plt.scatter(x[thresh].item(), y_on[thresh].item())
+
+    plt.grid()
     plt.legend()
     plt.xlabel("DNNSim")
     plt.title("Normalised histograms for on- and off-diagonal DNNSim values")
