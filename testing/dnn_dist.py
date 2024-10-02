198eb0d0f6eb2c927ac1acc2642e9d885be08cc1
suyash144 <suyashag@gmail.com>
2024-10-02 11:14:46 +0100

    can visualise threshold

--- testing/dnn_dist.py
+++ testing/dnn_dist.py
@@ -10,8 +10,8 @@ def get_threshold(mt_path:str, vis:bool=True):
     mt = mt.loc[(mt["RecSes1"]==mt["RecSes2"]), :]              # Only use within day rows to compute threshold
 
     # On-diagonal means same neuron. Off-diagonal means different neurons.
-    on_diag = mt.loc[(mt["RecSes1"]==mt["RecSes2"]) & (mt["ID1"]==mt["ID2"]), ["DNNSim"]]
-    off_diag = mt.loc[((mt["RecSes1"]==mt["RecSes2"]) & (mt["ID1"]==mt["ID2"]))==False, ["DNNSim"]]
+    on_diag = mt.loc[(mt["ID1"]==mt["ID2"]), ["DNNSim"]]
+    off_diag = mt.loc[(mt["ID1"]!=mt["ID2"]), ["DNNSim"]]
 
     # sanity check that the categories are being loaded correctly
     assert sum(on_diag.index.isin(off_diag.index)) == 0
@@ -42,13 +42,13 @@ def get_threshold(mt_path:str, vis:bool=True):
         plt.show()
     return x[thresh].item()
 
-def plot_hist(mt_path:str):
+def plot_hist(mt_path:str, thresh=None):
     mt = pd.read_csv(mt_path)
-    mt = mt.loc[(mt["RecSes1"]==mt["RecSes2"]), :]              # Only use within day rows to compute threshold
+    mt = mt.loc[(mt["RecSes1"]!=mt["RecSes2"]), :]              # Select the rows we want to use
 
     # On-diagonal means same neuron. Off-diagonal means different neurons.
-    on_diag = mt.loc[(mt["RecSes1"]==mt["RecSes2"]) & (mt["ID1"]==mt["ID2"]), ["DNNSim"]]
-    off_diag = mt.loc[((mt["RecSes1"]==mt["RecSes2"]) & (mt["ID1"]==mt["ID2"]))==False, ["DNNSim"]]
+    on_diag = mt.loc[(mt["ID1"]==mt["ID2"]), ["DNNSim"]]
+    off_diag = mt.loc[(mt["ID1"]!=mt["ID2"]), ["DNNSim"]]
 
     # sanity check that the categories are being loaded correctly
     assert sum(on_diag.index.isin(off_diag.index)) == 0
@@ -56,6 +56,9 @@ def plot_hist(mt_path:str):
     # visualise the results
     plt.hist(on_diag["DNNSim"], bins = 500, alpha = 0.5, density=True, label="On diagonal")
     plt.hist(off_diag["DNNSim"], bins = 500, alpha = 0.5, density=True, label="Off diagonal")
+    if thresh:
+        # if a threshold is provided then plot it as a vertical line.
+        plt.axvline(x=thresh)
 
     plt.grid()
     plt.legend()
@@ -69,5 +72,5 @@ test_data_root = r"C:\Users\suyas\R_DATA_UnitMatch"
 # mt_path = os.path.join(test_data_root, "AL031", "19011116684", "1", "new_matchtable.csv")
 mt_path = os.path.join(test_data_root, "AL036", "19011116882", "3", "new_matchtable.csv")       # 2497 neurons
 
-thresh = get_threshold(mt_path, vis=True)
-# plot_hist(mt_path)
+thresh = get_threshold(mt_path, vis=False)
+plot_hist(mt_path, thresh)
